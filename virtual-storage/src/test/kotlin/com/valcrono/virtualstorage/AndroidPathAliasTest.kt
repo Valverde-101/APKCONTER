04422792823ab1616d7c90d589a5aa70001ea635
Valverde <96694746+Valverde-101@users.noreply.github.com>
2026-07-17 15:26:03 -0500

    Revert "Runtime session schema & tracking, external-install UI, settings and file-explorer enhancements"

--- virtual-storage/src/test/kotlin/com/valcrono/virtualstorage/AndroidPathAliasTest.kt
+++ virtual-storage/src/test/kotlin/com/valcrono/virtualstorage/AndroidPathAliasTest.kt
@@ -1,5 +1,4 @@
 package com.valcrono.virtualstorage
 import org.junit.Assert.*
 import org.junit.Test
-import java.nio.file.Files
-class AndroidPathAliasTest { @Test fun sdcardAliasIsNormalizedSafely(){ assertEquals("/storage/emulated/0/Shared", VirtualFsResolver(VirtualFsNamespace(Files.createTempDirectory("vfs-test").toFile())).normalize("/sdcard/Shared")) }; @Test fun traversalRejected(){ assertThrows(IllegalArgumentException::class.java) { VirtualFsResolver(VirtualFsNamespace(Files.createTempDirectory("vfs-test").toFile())).normalize("/sdcard/../data") } } }
+class AndroidPathAliasTest { @Test fun sdcardAliasIsNormalizedSafely(){ assertEquals("/storage/emulated/0/Shared", VirtualFsResolver(VirtualFsNamespace(createTempDir())).normalize("/sdcard/Shared")) }; @Test fun traversalRejected(){ assertThrows(IllegalArgumentException::class.java) { VirtualFsResolver(VirtualFsNamespace(createTempDir())).normalize("/sdcard/../data") } } }
--- virtual-storage/src/test/kotlin/com/valcrono/virtualstorage/VirtualFsAppIsolationTest.kt
+++ virtual-storage/src/test/kotlin/com/valcrono/virtualstorage/VirtualFsAppIsolationTest.kt
@@ -1,6 +1,5 @@
 package com.valcrono.virtualstorage
 import org.junit.Assert.*
 import org.junit.Test
-import java.nio.file.Files
 import java.io.File
-class VirtualFsAppIsolationTest { @Test fun appsCannotReadEachOther(){ val r=Files.createTempDirectory("vfs-test").toFile(); File(r,"virtual/users/0/a.b.c/data").mkdirs(); File(r,"virtual/users/0/d.e.f/data").mkdirs(); val fs=VirtualFileSystem(VirtualFsNamespace(r)); assertThrows(IllegalArgumentException::class.java) { fs.resolve("/data/data/d.e.f", VirtualFsAccessContext.app("a.b.c")) }; assertThrows(IllegalArgumentException::class.java) { fs.resolve("/data/data/a.b.c", VirtualFsAccessContext.app("d.e.f")) }; assertEquals("a.b.c", fs.resolve("/data/data/a.b.c", VirtualFsAccessContext.admin()).packageName) } }
+class VirtualFsAppIsolationTest { @Test fun appsCannotReadEachOther(){ val r=createTempDir(); File(r,"virtual/users/0/a.b.c/data").mkdirs(); File(r,"virtual/users/0/d.e.f/data").mkdirs(); val fs=VirtualFileSystem(VirtualFsNamespace(r)); assertThrows(IllegalArgumentException::class.java) { fs.resolve("/data/data/d.e.f", VirtualFsAccessContext.app("a.b.c")) }; assertThrows(IllegalArgumentException::class.java) { fs.resolve("/data/data/a.b.c", VirtualFsAccessContext.app("d.e.f")) }; assertEquals("a.b.c", fs.resolve("/data/data/a.b.c", VirtualFsAccessContext.admin()).packageName) } }
